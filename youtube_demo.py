74d74af06c33234ad9a9bff98cca192a34b7994c
deep-ink-ventures <chp@deep-ink.ventures>
2023-06-02 15:05:00 +0200

    cleanup

--- youtube_demo.py
+++ youtube_demo.py
@@ -5,15 +5,12 @@ from pytube import YouTube
 
 with BytesIO() as tmp_file:
     YouTube("https://www.youtube.com/watch?v=ui7f4OQkMgU").streams.first().stream_to_buffer(tmp_file)
-    tmp_file.seek(0)
 
+    # sha256 hash of the file
+    tmp_file.seek(0)
     m = hashlib.sha256()
     m.update(tmp_file.getbuffer())
+
     video_hash = m.hexdigest()
 
 print(video_hash)
-
-
-
-
-
