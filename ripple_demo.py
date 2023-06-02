7f8597836abd0a027df53a12f7de1eb720e5793c
deep-ink-ventures <chp@deep-ink.ventures>
2023-06-02 14:48:45 +0200

    cleanup

--- ripple_demo.py
+++ ripple_demo.py
@@ -5,13 +5,11 @@ from xrpl.wallet import generate_faucet_wallet
 from xrpl.models.requests import AccountNFTs
 from xrpl.clients import JsonRpcClient
 
-
 TESTNET_RPC_URL = "https://s.altnet.rippletest.net:51234/"
 xrp_client = JsonRpcClient(TESTNET_RPC_URL)
 
 # Let's create a prefunded test account
 issuer_wallet = generate_faucet_wallet(xrp_client, debug=True)
-print(issuer_wallet)
 
 # Construct NFTokenMint transaction to mint 1 NFT
 mint_tx = NFTokenMint(
@@ -23,8 +21,6 @@ mint_tx = NFTokenMint(
 
 # Sign mint_tx using the issuer account
 mint_tx_signed = safe_sign_and_autofill_transaction(transaction=mint_tx, wallet=issuer_wallet, client=xrp_client)
-
-# mint tx result has all sorts of fun information but we can ignore it for now
 mint_tx_result = send_reliable_submission(transaction=mint_tx_signed, client=xrp_client).result
 
 # Query the minted account for its NFTs
@@ -33,10 +29,12 @@ get_account_nfts = xrp_client.request(
 )
 
 for nft in get_account_nfts.result['account_nfts']:
-    print(nft)
-    print(f"\nNFToken metadata:"
-          f"\n    Issuer: {nft['Issuer']}"
-          f"\n    NFT ID: {nft['NFTokenID']}"
-          f"\n    NFT Taxon: {nft['NFTokenTaxon']}"
-          f"\n    NFT URI: {hex_to_str(nft['URI'])}"
-      )
+    print(
+        f"\nIssuer: {nft['Issuer']}"
+        f"\nNFT ID: {nft['NFTokenID']}"
+        f"\nNFT Taxon: {nft['NFTokenTaxon']}"
+        f"\nNFT URI: {hex_to_str(nft['URI'])}"
+    )
+
+print(f"\nMint TX: https://testnet.xrpl.org/transactions/{mint_tx_result['hash']}")
+print(f"\All NFTs: https://testnet.xrpl.org/accounts/{issuer_wallet.classic_address}/assets/nft")
